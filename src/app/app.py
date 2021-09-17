65768dc91ac4f253185368aa27585d62d346e7b6
RCSnyder <44173311+RCSnyder@users.noreply.github.com>
2021-09-16 21:33:36 -0500

    change structure

--- src/app/app.py
+++ src/app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "This is an example app"
+    return render_template('index.html')
