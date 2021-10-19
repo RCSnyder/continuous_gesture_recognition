2dc7c09d16a03fee575e09e3f914502550d58d6b
Ian Zurutuza <ian.zurutuza@gmail.com>
2021-10-19 17:57:11 -0500

    remove rounding here

--- test_newbackend/utils/fps.py
+++ test_newbackend/utils/fps.py
@@ -11,6 +11,6 @@ class FPS:
     def __call__(self):
         self.frametimestamps.append(time.time())
         if(len(self.frametimestamps) > 1):
-            return round(len(self.frametimestamps)/(self.frametimestamps[-1]-self.frametimestamps[0]), 2)
+            return len(self.frametimestamps)/(self.frametimestamps[-1]-self.frametimestamps[0])
         else:
             return 0.0
